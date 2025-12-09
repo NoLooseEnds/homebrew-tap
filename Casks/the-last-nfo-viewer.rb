ce067e2d65cc2e9a7ac2df71393179918a5fb09b
NoLooseEnds <>
2025-12-09 10:48:52 +0100

    Initial commit of Homebrew Tap

---
+++ Casks/the-last-nfo-viewer.rb
@@ -0,0 +1,20 @@
+cask "the-last-nfo-viewer" do
+  version "1.0.0"
+  sha256 "ffc35c01fe9367d2350a948bed4daa3efb303d529df7ce5b6bdd77e89990d8d6"
+
+  url "https://github.com/NoLooseEnds/the-last-nfo-viewer/releases/download/v#{version}/TheLastNFOViewer.zip"
+  name "The Last NFO Viewer"
+  desc "NFO file viewer"
+  homepage "https://github.com/NoLooseEnds/the-last-nfo-viewer"
+
+  app "The Last NFO Viewer.app"
+
+  zap trash: [
+    "~/.the-last-nfo-viewer.json",
+    "~/Library/Caches/com.nle.TheLastNFOViewer",
+    "~/Library/Containers/com.nle.TheLastNFOViewer",
+    "~/Library/Containers/com.nle.TheLastNFOViewer.QuickLook",
+    "~/Library/Preferences/com.nle.TheLastNFOViewer.plist",
+    "~/Library/Saved Application State/com.nle.TheLastNFOViewer.savedState",
+  ]
+end
---
+++ README.md
@@ -0,0 +1,9 @@
+# NoLooseEnds Homebrew Tap
+
+This is a personal Homebrew Tap for [The Last NFO Viewer](https://github.com/NoLooseEnds/the-last-nfo-viewer).
+
+## Installation
+
+```bash
+brew install NoLooseEnds/tap/the-last-nfo-viewer
+```
